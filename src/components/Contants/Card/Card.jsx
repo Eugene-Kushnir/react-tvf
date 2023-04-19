b3a5db4ef70a0aed2d3a6a5a7f17564d0121d0ad
Eugene Kushnir <kushnir.fort@gmail.com>
2023-04-19 10:35:27 +0300

    create local db and show card

--- src/components/Contants/Card/Card.jsx
+++ src/components/Contants/Card/Card.jsx
@@ -1,17 +1,17 @@
-const Card = ()=> {
+const Card = (props)=> {
    return (
-      <div className="card">
-         <img src="/img/0.jpg" alt="pfoto" />
-         <img  className = "card_like" src="/img/like.png" alt="like" />
+         <div className="card">
+         <img src={props.photo} alt="pfoto" />
+         <img  className = "card_like" src="/img/like.png" alt="like" onClick={() => alert('like')} />
          <div className="card_categories">
-            <span className="card_category">Продаж</span>
-            <span className="card_sub_category">Будинок</span>
+            <span className="card_category">{props.card_category}</span>
+            <span className="card_sub_category">{props.card_sub_category}</span>
          </div>
-         <h2>Продам дачу в СТ Сосновий бір</h2>
-         <span className="card_price"><b>39900$</b></span>
+         <h2>{props.titl}</h2>
+         <span className="card_price"><b>{props.price}$</b></span>
          <div className="card_footer">
-            <span className="card_rooms">3 кімнати</span>
-            <span className="card_m2">113 <img src="./img/measurement.png" alt="m2" /></span>
+            <span className="card_rooms">{props.rooms} кімнати</span>
+            <span className="card_m2">{props.m2} <img src="./img/measurement.png" alt="m2" /></span>
          </div>
       </div>
    )
--- src/components/Contants/Contants.jsx
+++ src/components/Contants/Contants.jsx
@@ -2,16 +2,98 @@ import Card from './Card/Card';
 import Head from './Head/Head';
 import Obj from './Obj/Obj';
 
+const cards = [
+   {
+      photo: "/img/0.jpg",
+      card_category: 'Продаж',
+      card_sub_category: 'Будинок',
+      title: 'Продам дачу в СТ Сосновий бір',
+      price: '39900',
+      rooms: '3',
+      m2: '113'
+   },
+   {
+      photo: "/img/1.jpg",
+      card_category: 'Продаж',
+      card_sub_category: 'Дача',
+      title: 'Продам дом в Новомосковске',
+      price: '125000',
+      rooms: '6',
+      m2: '210'
+   },
+   {
+      photo: "/img/1.jpg",
+      card_category: 'Продаж',
+      card_sub_category: 'Дача',
+      title: 'Продам дом в Новомосковске',
+      price: '125000',
+      rooms: '6',
+      m2: '210'
+   },
+   {
+      photo: "/img/1.jpg",
+      card_category: 'Продаж',
+      card_sub_category: 'Дача',
+      title: 'Продам дом в Новомосковске',
+      price: '125000',
+      rooms: '6',
+      m2: '210'
+   },
+   {
+      photo: "/img/0.jpg",
+      card_category: 'Продаж',
+      card_sub_category: 'Будинок',
+      title: 'Продам дачу в СТ Сосновий бір',
+      price: '39900',
+      rooms: '3',
+      m2: '113'
+   },
+   {
+      photo: "/img/1.jpg",
+      card_category: 'Продаж',
+      card_sub_category: 'Дача',
+      title: 'Продам дом в Новомосковске',
+      price: '125000',
+      rooms: '6',
+      m2: '210'
+   },
+   {
+      photo: "/img/1.jpg",
+      card_category: 'Продаж',
+      card_sub_category: 'Дача',
+      title: 'Продам дом в Новомосковске',
+      price: '125000',
+      rooms: '6',
+      m2: '210'
+   },
+   {
+      photo: "/img/1.jpg",
+      card_category: 'Продаж',
+      card_sub_category: 'Дача',
+      title: 'Продам дом в Новомосковске',
+      price: '125000',
+      rooms: '6',
+      m2: '210'
+   },
+];
 
 const Contants = () => {
    return (
       <main className="content">
         <Head/>
         <section className="cards">
-          <Card/>
-          <Card/>
-          <Card/>
-          <Card/>
+        {cards.map((e) => (
+         <Card
+          photo = {e.photo}
+          card_category = {e.card_category}
+          card_sub_category = {e.card_sub_category}
+          title = {e.title}
+          price = {e.price}
+          rooms = {e.rooms}
+          m2 = {e.m2}
+          />
+        ))}
+        )
         </section>
          <Obj/>
       </main>
